b03d0ea2388b9d93352f133c44247eabd89d482b
ae458 <ae458@njit.edu>
2024-04-25 18:52:28 -0400

    fix

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Westwood-18@localhost/cars_dealershipx' #Abdullah Connection
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:great-days321@localhost/cars_dealershipx' #Dylan Connection 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:A!19lopej135@localhost/cars_dealershipx' # joan connection
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12340@192.168.56.1/cars_dealershipx'# Ismael connection
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12340@192.168.56.1/cars_dealershipx'# Ismael connection
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:*_-wowza-shaw1289@localhost/cars_dealershipx' #hamza connection
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:42Drm400$!@localhost/cars_dealershipx'
 
@@ -466,7 +466,8 @@ def get_customer_bank_details():
             'bank_name': bank.bank_name,
             'account_number': bank.account_number,
             'routing_number': bank.routing_number,
-            'customer_id': bank.customer_id
+            'customer_id': bank.customer_id,
+            'credit_score': bank.credit_score
         }
         for bank in bank_details
     ]
